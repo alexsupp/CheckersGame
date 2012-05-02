1d63978c98c6c5d5c826d090b08bc3e621e916e3
Jacob Duell <djacob@vt.edu>
2012-05-02 19:59:58 -0400

    Added double jump capability and end of game criteria

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ void instructions()
 	"Pieces may only move forward until they reach the opposite side/n"<<
 	"If a piece reaches the opposite side it becomes a KING and can/n"<<
 	"move in any direction. State your moves with the letter column first/n"<<
-	"then the row number. i.e. Move: A5 A6"<<endl;
+	"then the row number. i.e. Move: A5 A6"<<
+	"Jumps are made by specifying a move of two diagonal spaces over an opposing piece"<<
+	"You win the game when you have jumped all of your opponents pieces"<<endl;
 }
 
 int convertLetter(char a)
@@ -39,10 +41,12 @@ int main()
 		redTurn
 	};
 	turns turn = redTurn;
+	int moveResult = 0;
 	int redPieces = 12;
 	int blackPieces = 12;
 	bool gameOver = false;
 	bool invalid = false;
+	bool doublejump = false;
 	Grid gridBoard;
 	char from[2], to[2];
 	int fromPos[2], toPos[2];
@@ -54,15 +58,24 @@ int main()
 		std::cout << gridBoard << endl;
 		if(invalid)
 			std::cout << "Invalid move, please try again" <<endl;
-		std::cout << "Enter move ";
+		if(doublejump)
+		{
+			std::cout << "You must double jump from " << fromPos[0] << fromPos[1] << " ";
+		}
+		else
+			std::cout << "Enter move ";
 		if(turn==blackTurn)
 			cout<<"Black: ";
 		else
 			cout<<"Red: ";
 		std::cin >> from >> to;
 
-		fromPos[0] = convertLetter(from[0]);
-		fromPos[1] = convertNumber(from[1]);
+		if(!doublejump)
+		{
+			fromPos[0] = convertLetter(from[0]);
+			fromPos[1] = convertNumber(from[1]);
+		}
+
 		toPos[0] = convertLetter(to[0]);
 		toPos[1] = convertNumber(to[1]);
 
@@ -75,29 +88,68 @@ int main()
 		cout<<"ALSO MOVE FROM " << fromPos[0] << "," << fromPos[1] << " to " << toPos[0] << "," << toPos[1] << endl;
 		if(!invalid)
 		{
+			moveResult = gridBoard.move(fromPos, toPos) == -1);
 			switch(turn)
 			{
 				case blackTurn:
-					if((gridBoard.getPosition(fromPos[0],fromPos[1]).color != BLACK)
-						|| (gridBoard.move(fromPos, toPos) == -1))
+					if(gridBoard.getPosition(fromPos[0],fromPos[1]).color != BLACK || moveResult == -1)
 					{
 						invalid = true;
 						break;
 					}
+					if(moveResult ==  1)
+						redPieces--;
 					turn = redTurn;
 					break;
 
 				case redTurn:
-					if((gridBoard.getPosition(fromPos[0],fromPos[1]).color != RED)
-						|| (gridBoard.move(fromPos, toPos) == -1))
+					if(gridBoard.getPosition(fromPos[0],fromPos[1]).color != RED || moveResult == -1)
 					{
 						invalid = true;
 						break;
+					}
+					if(moveResult == 1)
+					{
+						doublejump = false;
+						blackPieces--;
+
+						if(gridBoard.getPosition(toPos[0],toPos[1]).type == KING)
+						{
+							if(gridBoard.getPosition(toPos[0]-1,toPos[1]+1).color == BLACK)
+							{
+								if(gridBoard.getPosition(toPos[0]-2,toPos[1]-2).color == NONE)
+									doublejump = true;
+							}
+							if(gridBoard.getPosition(toPos[0]+1,toPos[1]+1).color == BLACK)
+							{
+								if(gridBoard.getPosition(toPos[0]-2,toPos[1]-2).color == NONE)
+									doublejump = true;
+							}
+						}
+						if(gridBoard.getPosition(toPos[0]-1,toPos[1]-1).color == BLACK)
+						{
+							if(gridBoard.getPosition(toPos[0]-2,toPos[1]-2).color == NONE)
+								doublejump = true;
+						}
+						if(gridBoard.getPosition(toPos[0]+1,toPos[1]-1).color == BLACK)
+						{
+							if(gridBoard.getPosition(toPos[0]+2,toPos[1]-2).color == NONE)
+								doublejump = true;
+						}
 					}
 					turn = blackTurn;
 					break;
 			}//end turn switch
 		}//end invalid check
+		if(blackPieces == 0 || redPieces == 0)
+			gameOver == true;
 	}//end while
+
+	if(blackPieces == 0)
+		std::cout << "RED WINS!"<<endl;
+	if(redPieces == 0)
+		std::cout << "BLACK WINS!"<<endl;
+	if(blackPieces != 0 && redPieces != 0)
+		std::cout << "NOBODY WINS :("<<endl;
 	return 0;
 }
